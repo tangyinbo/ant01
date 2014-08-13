6ef4d445ac6177987fa5ce421f8efe877a971fa0
tangyinbo <canton_cowboy@super_pc>
2014-08-13 23:00:33 +0800

    ant  init

---
+++ build/src/cn/edu/ant/HelloAnt.java
@@ -0,0 +1,7 @@
+package cn.edu.ant;
+
+public class HelloAnt {
+	public static void main(String[] args) {
+		System.out.println("hello ant");
+	}
+}
---
+++ src/cn/edu/ant/TestExclude.java
@@ -0,0 +1,5 @@
+package cn.edu.ant;
+
+public class TestExclude {
+
+}
